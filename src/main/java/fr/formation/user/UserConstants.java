4969e5459bc6ae8a5ffecf5a3a28a68722b484e8
AlexandreR <alexandre.ramade@sfr.fr>
2019-07-05 14:22:03 +0200

    Signup User : validation password and tests

---
+++ src/main/java/fr/formation/user/UserConstants.java
@@ -0,0 +1,18 @@
+package fr.formation.user;
+
+public class UserConstants {
+
+    public static final String PASSWORD_PATTERN = "((?=.*[a-z])(?=.*\\d)(?=.*[A-Z]).{8,50})";
+
+    public static final int MIN_LENGTH = 2;
+
+    public static final int USERNAME_MAX_LENGTH = 50;
+
+    public static final int EMAIL_MAX_LENGTH = 100;
+
+    public static final int CITY_MAX_LENGTH = 50;
+
+
+
+
+}
