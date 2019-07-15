219f5f3452a94d2c28afac864ebe641c9aa6d461
Melodie <meisselmelodie@gmail.com>
2019-07-15 17:51:35 +0200

    Ajout des liens des images pour les photos des artistes

--- src/main/java/fr/formation/BoostrapData.java
+++ src/main/java/fr/formation/BoostrapData.java
@@ -19,6 +19,8 @@ import java.util.Arrays;
 @Component
 public class BoostrapData {
 
+	private final static String DIR_IMAGES_ARTIST = "/src/assets/";
+
 	private UserService userService;
 
 	private ArtistService artistService;
@@ -145,7 +147,7 @@ public class BoostrapData {
 
 		artistDetailService.addNewArtistDetail(
 				artistService.findByUsername("planb"),
-				"photo",
+				DIR_IMAGES_ARTIST+"planb.jpg",
 				"On a toujours besoin d'un plan B... Plan B est un groupe Marseillais de reprises pop rock années 70 et 80 formé en 2012.   \n" +
 						"\n" +
 						"Nous reprenons des titres que nous aimons et qui nous ont fait rêver  ou danser. Que des valeurs sures, que des classiques (Téléphone, The  Rolling Stones, ZZ TOP, The Beatles, The Doors...). \n" +
@@ -158,7 +160,7 @@ public class BoostrapData {
 
 		artistDetailService.addNewArtistDetail(
 				artistService.findByUsername("peoplearestrange"),
-				"photo",
+				DIR_IMAGES_ARTIST+"peoplearestrange.jpg",
 				"People Are Strange,  c est avant tout un couple : Laurent et Tina. Passionnés tous deux de musique rock 70's , ils y puisent leurs influences et leurs inspirations ( Led Zeppelin,  Janis Joplin,  The Doors... .) En duo, ils reprennent des morceaux phares de cette époque (Dust in the wind, my lady d'Arbanville, Me and Bobby Mc Gee, Whole lotta love... )  ainsi que des morceaux des années 50/60  avec des artistes comme Elvis Presley,  Chuck Berry, The Beatles,  The Rolling stones...  En groupe rock, le repertoire s'agremente de chansons plus modernes ( ACDC/ Red hot chili peppers / Rage against the machine/ Scorpions / Kiss... )  Enfin, passionnés de musique celtique, ils proposent un répertoire traditionnel irlandais avec des standards tels que Sally Mc Lennane, Dirty old town, Irish rover et des jigs instrumentaux.",
 				"peoplearestrange.monsite.fr",
 				612345678,
@@ -167,18 +169,18 @@ public class BoostrapData {
 
 		artistDetailService.addNewArtistDetail(
 				artistService.findByUsername("barriocombo"),
-				"photo",
+				DIR_IMAGES_ARTIST+"barrioCombo.jpg",
 				"Apportez de la couleur à votre évènement grâce au groupe latino Barrio Combo. Composé de musiciens professionnels, ce groupe saura insuffler musique et bonne humeur à votre événement pour que vous en gardiez des souvenirs uniques.\n" +
 						"\n" +
 						"Barrio Combo, ce sont des musiciens du barrio qui se sont réunis afin de partager leur passion pour la musique latine. Ils vous offrent une animation idéale pour votre cocktail ou vin d'honneur ou un concert. Ils sauront ensoleiller votre réception.",
 				"barriocombo.monsite.fr",
 				612347878,
-				new ArrayList<String>(Arrays.asList("Rhône"))
+				new ArrayList<String>(Arrays.asList("Rhône","Isère"))
 		);
 
 		artistDetailService.addNewArtistDetail(
 				artistService.findByUsername("echo"),
-				"photo",
+				DIR_IMAGES_ARTIST+"echo.jpg",
 				"Fondé en 2014 par Charlotte Silvestri, Guillaume Plasse et Colas Hutter, le trio acoustique s'est d'abord spécialisé dans l'arrangement, jouant avec les contrastes de styles et d'atmosphères, afin d'apporter quelque chose de nouveau et de personnel aux tubes intemporels composant leur répertoire.\n" +
 						"\n" +
 						"Aussi calme et langoureux qu'il sait être rythmé et dynamique, le tandem basse - guitare sublime chacune de ses interprétations par la voix envoûtante de leur chanteuse, donnant aux morceaux des textures originales et inattendues",
@@ -189,7 +191,7 @@ public class BoostrapData {
 
 		artistDetailService.addNewArtistDetail(
 				artistService.findByUsername("coverclub"),
-				"photo",
+				DIR_IMAGES_ARTIST+"coverclub.jpg",
 				"Cover Club est un groupe de reprises pop-rock basé à Lyon, disponible pour animer vos événements publics ou privés.\n" +
 						"\n" +
 						"Cover Club écume les scènes sans relâche et avec succès depuis 2013, à grand coup de reprises bien senties, de groove tenace et de refrains à reprendre en chœur.",
